ec97d0ce06ba2947d83cad8d3e8b247650a0db06
deanApostolakos <81169512+deanApostolakos@users.noreply.github.com>
2021-04-04 11:58:21 -0400

    added customerinfo method and variables

--- main.java
+++ main.java
@@ -23,7 +23,21 @@ class CustomerSystem{
             if (userInput.equals(enterCustomerOption)){
                 // Only the line below may be editted based on the parameter list and how you design the method return
 		        // Any necessary variables may be added to this if section, but nowhere else in the code
-                enterCustomerInfo();
+                
+                // Assign variables for all inputs of the enter customer info method
+                String firstName = "firstName";
+                String lastName = "lastName";
+                String city = "city"; 
+                String postalCode = "postalCode";
+                String creditCard = "creditCard";
+                
+                // Call method for each individual input
+                firstName = enterCustomerInfo(firstName);
+                lastName = enterCustomerInfo(lastName);
+                city = enterCustomerInfo(city);
+                postalCode = enterCustomerInfo(postalCode);
+                creditCard = enterCustomerInfo(creditCard);
+
             }
             else if (userInput.equals(generateCustomerOption)) {
                 // Only the line below may be editted based on the parameter list and how you design the method return
@@ -53,8 +67,49 @@ class CustomerSystem{
     * The method may not nesessarily be a void return type
     * This method may also be broken down further depending on your algorithm
     */
-    public static void enterCustomerInfo() {
+    public static String enterCustomerInfo(String variable) {
+        // Initialize scanner
+        Scanner reader = new Scanner(System.in);
+
+        // Customer first name input
+        if (variable.equals("firstName")){
+            System.out.print("Customer First Name        : ");
+            String fName = reader.nextLine();
+            variable = fName;
+        }
+        // Customer last name input
+        else if (variable.equals("lastName")){
+            System.out.print("Customer Last Name         : ");
+            String lName = reader.nextLine();
+            variable = lName;
+        }
+        // Customer city input
+        else if (variable.equals("city")){
+            System.out.print("Customer City              : ");
+            String cty = reader.nextLine();
+            variable = cty;
+        }
+        // Customer postal code input
+        else if (variable.equals("postalCode")){
+            System.out.print("Customer Postal Code       : ");
+            String pCode = reader.nextLine();
+            variable = pCode;
+        }
+        // Customer Credit card input
+        else if (variable.equals("creditCard")){
+            System.out.print("Customer Credit Card Number: ");
+            String card = reader.nextLine();
+            variable = card;
+        }
+        
+        // The method doesn't work if the reader is closed. I don't know why. 
+        // I will leave it commented out though.
+        //reader.close();
+
+        // Return input to main
+        return variable;
     }
+
     /*
     * This method may be edited to achieve the task however you like.
     * The method may not nesessarily be a void return type
